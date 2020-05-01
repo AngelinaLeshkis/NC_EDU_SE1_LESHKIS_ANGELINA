4618d8790fcbee2018cfa2647fba72fcac6dd75b
Angelina Leshkis <Aleshkis@mail.ru>
2020-05-01 13:26:40 +0300

    Add post service

--- Fapi/src/main/java/fapi/service/PostService.java
+++ Fapi/src/main/java/fapi/service/PostService.java
@@ -1,4 +1,19 @@
 package fapi.service;
 
-public class PostService {
+import fapi.model.Post;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PostService {
+
+    //Post savePost(CreatePostDTO post);
+
+    Iterable<Post> getPostsByUserId(Long id);
+
+    void deletePostByPostId(Long id);
+
+    Post getPostByPostId(Long id);
+
+    Iterable<Post> getPosts();
 }
