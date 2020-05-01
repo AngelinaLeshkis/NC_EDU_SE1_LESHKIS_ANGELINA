4c2f17e25e299dc9e5e993f8038ff14c9abede3d
Angelina Leshkis <Aleshkis@mail.ru>
2020-05-01 13:23:54 +0300

    Add createPostDTO

---
+++ Backend/src/main/java/com/dto/CreatePostDTO.java
@@ -0,0 +1,43 @@
+package com.dto;
+
+import java.util.Date;
+
+public class CreatePostDTO {
+
+    private Long id;
+    private String text;
+    private Date date;
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
